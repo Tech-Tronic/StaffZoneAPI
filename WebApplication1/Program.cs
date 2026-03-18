ca9605422a800cfd805310f8935151cf5b23366c
YRQD <amirashraf012080@gmail.com>
2026-03-18 23:12:04 +0200

    program.cs cleanup

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -14,46 +14,25 @@ builder.Services.AddApplicationManagers();
 new MapperConfiguration(cfg => cfg.AddProfile<MappingProfile>())
 	.AssertConfigurationIsValid();
 
-// AutoMapper - Singleton (configured once)
-// IMapper interface that you inject into your controllers or services is registered as Transient by default.
+
 builder.Services.AddAutoMapper(typeof(MappingProfile));
-//builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 builder.Services.AddControllers()
 	.AddJsonOptions(options =>
 {
-	// This makes all enums responses serialize as text globally.
 	options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
 });
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 
 builder.Services.AddDbContext<StaffZoneContext>(options =>
 	options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQLConnection")));
 
-/*builder.Services.Configure<ApiBehaviorOptions>(options =>
-{
-	options.DisableImplicitFromServicesParameters = true;
-});*/
-
-//Add your custom service
-//builder.Services.AddScoped<IEmailService, EmailService>();
-//builder.Services.AddSingleton<ICacheService, RedisCacheService>();
-
-builder.Services.AddCors(options =>
-{
-	options.AddPolicy("AllowFrontend", policy =>
-	{
-		policy.WithOrigins("http://localhost:3000")  // Your frontend URL
-			  .AllowAnyHeader()
-			  .AllowAnyMethod();
-	});
-});
-
 var app = builder.Build();
 
-// Add global exception handling middleware
+#region Middlewaer Pipeline
+
 app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
 
 if (app.Environment.IsDevelopment())
@@ -63,15 +42,14 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-	// Only redirect to HTTPS in production
 	app.UseHttpsRedirection();
 }
 
-app.UseCors("AllowFrontend");
-
-app.UseAuthorization();
-
 app.MapControllers();
+app.UseRouting();
 
+#endregion
+
+app.MapGet("/hello", () => "Hello World!");
 
 app.Run();
